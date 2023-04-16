0476e4addf077a05e268c5ef9b886607a608a3d7
Emrullah58 <emrullah_58@hotmail.de>
2023-04-16 23:46:18 +0200

    Helloworld

--- Dashboard.py
+++ Dashboard.py
@@ -2,7 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 app = Dash(__name__)
 
 app.layout = html.Div([
