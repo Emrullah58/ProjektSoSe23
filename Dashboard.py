d37470eacbe04497962e77f36513d21109814438
NellyPena <117836534+NellyPena@users.noreply.github.com>
2023-04-25 01:29:30 +0200

    Update Dashboard.py
    + ein wenig Design

--- Dashboard.py
+++ Dashboard.py
@@ -8,17 +8,23 @@ df2 = pd.read_csv('DAX.csv')
 
 app = Dash(__name__)
 
-fig = px.line(df, x='Date', y="Open")
+fig = px.line(df, x='Date', y="Open", title= 'Microsoft')
 fig1 = px.line(df1, x='Date', y="Open")
 fig2 = px.line(df2, x='Date', y="Open")
 
 
 app.layout = html.Div([
-    html.H1(children='Stock Prices', style={'textAlign':'center'}),
+    html.H1(children='Aktienkurse', style={'textAlign':'center'}),
+    html.Hr(),
+    html.P(children= "Man kann zwischen verschiedenen Preisen wählen."
+                    " Alle Diagramme werden mit der Auswahl aktualisiert."),
     html.Hr(),
     dcc.RadioItems(options=['Open', 'High', 'Low','Close'], value='Open', id='controls-and-radio-item'),
+    html.H1(children = 'Microsoft (MSFT)'),
     dcc.Graph(id='graph-content', figure = fig),
+    html.H1(children= 'Tesla (TSL)'),
     dcc.Graph(id='graph-content1', figure = fig1),
+    html.H1(children='DAX'),
     dcc.Graph(id='graph-content2', figure = fig2)
 ])
 
@@ -53,6 +59,7 @@ def update_graph(col_chosen):
     #fig1 = px.line(df1,x='Date',y=col_chosen)
     fig2 = px.line(df2,x='Date',y=col_chosen)
     return fig2
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
